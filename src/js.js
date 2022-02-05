426cf6baf946566d35936b3e5ac00a7b45836a23
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 12:31:32 +0530

    Escape Sequences in Strings

--- src/js.js
+++ src/js.js
@@ -96,4 +96,6 @@ c /=  11;
 
 // const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; 
