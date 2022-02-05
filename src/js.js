e5df3170e2bfea10039ffedc3c7eb38b79961748
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:41:56 +0530

    order of if else

--- src/js.js
+++ src/js.js
@@ -556,3 +556,17 @@ function nextInLine(arr, item) {
   }
   
   testElseIf(7);
+
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  // orderMyLogic(7);
+  console.log(orderMyLogic(4));
