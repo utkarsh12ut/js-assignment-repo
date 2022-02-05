bfedbf7ca139d95894ac1243c1840e3942879657
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:13:31 +0530

    Comparisons with the Logical And Operator

--- src/js.js
+++ src/js.js
@@ -493,3 +493,17 @@ function nextInLine(arr, item) {
   }
   
   testLessOrEqual(10);
+
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val > 24 && val < 51){
+        return "Yes";
+      
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
