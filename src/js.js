96538c0e81f8c35a19d962fa811f149db1c9934b
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:02:32 +0530

    Comparison with the Greater Than Operator

--- src/js.js
+++ src/js.js
@@ -402,3 +402,64 @@ function nextInLine(arr, item) {
   
   compareEquality(10, "10");
         
+
+
+  function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
+
+
+  function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
+
+  function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
+  function testGreaterOrEqual(val) {
+    if (val>=20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val>=10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
+
+  function testGreaterOrEqual(val) {
+    if (val>=20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val>=10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
