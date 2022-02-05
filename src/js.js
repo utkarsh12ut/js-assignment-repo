d88c0adf76c7f16e446cde92fb97257be505d166
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:54:50 +0530

    Chaining If Else Statements

--- src/js.js
+++ src/js.js
@@ -570,3 +570,30 @@ function nextInLine(arr, item) {
   
   // orderMyLogic(7);
   console.log(orderMyLogic(4));
+
+
+  function testSize(num) {
+    // Only change code below this line
+  if(num<5){
+    return "Tiny";
+  
+  }else if(num<10){
+    return "Small";
+  }
+  else if(num<15){
+  
+  return "Medium";
+  }
+  else if(num<20){
+    return "Large";
+  }
+  
+  else{
+    return "Huge";
+  }
+  
+    return "Change Me";
+    // Only change code above this line
+  }
+  
+  testSize(7);
