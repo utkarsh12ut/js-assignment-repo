b6b1e0394e423f33bbbb95c267d5fffb89a83caa
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 12:26:17 +0530

    Quoting Strings with Single Quotes

--- src/js.js
+++ src/js.js
@@ -94,4 +94,6 @@ c /=  11;
 
 
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
