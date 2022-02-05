75b82add685ac960e492ca2442583ad7ca96881b
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:30:46 +0530

    intro of else statement

--- src/js.js
+++ src/js.js
@@ -521,3 +521,22 @@ function nextInLine(arr, item) {
   
   // Change this value to test
   testLogicalOr(15);
+
+
+  function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  testElse(4);
