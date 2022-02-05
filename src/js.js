ffbfcfd19594fc07ce899c39a198ae55dd65d14c
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 16:23:40 +0530

    Comparisons with the Logical Or Operator

--- src/js.js
+++ src/js.js
@@ -507,3 +507,17 @@ function nextInLine(arr, item) {
   }
   
   testLogicalAnd(10);
+
+  function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val > 20 || val < 10) {
+  return "Outside";
+  }
+  
+  // Only change code above this line
+  return "Inside";
+  }
+  
+  // Change this value to test
+  testLogicalOr(15);
