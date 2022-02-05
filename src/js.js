8a75212fe0728eb4a94555349985a41b90704eb5
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 12:44:19 +0530

    Concatenating Strings with the Plus Equals Operator

--- src/js.js
+++ src/js.js
@@ -100,4 +100,8 @@ c /=  11;
 
 // const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; 
 
-const myStr = "This is the start. " +  "This is the end.";
+// const myStr = "This is the start. " +  "This is the end.";
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+console.log(myStr)
