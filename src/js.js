ae2ab49c2a119673cfd33b0c09b1cedd620320df
utkarsh12ut <utkarsh@cedcoss.com>
2022-02-05 15:21:36 +0530

    Understanding Undefined Value returned from a Function

--- src/js.js
+++ src/js.js
@@ -302,4 +302,21 @@ var outerWear= "sweater";
 }
 
 myOutfit();
+
+
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree(5);
+addFive(3);
         
